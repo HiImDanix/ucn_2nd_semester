4a9daa77d467e6b834cfe2e017f6df26d6465ea1
Daniels Kanepe <danixkanepe@gmail.com>
2022-04-13 16:14:36 +0200

    Implemented employee CRUD methods

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -48,6 +48,63 @@ public class EmployeeController {
     }
 
     /*
+     * Update employee's first name
+     */
+    public void updateEmployeeFirstName(Employee employee, String firstName) throws DataAccessException {
+        String oldFirstName = employee.getFirstName();
+        try {
+            employee.setFirstName(firstName);
+            employeeDB.updateEmployee(employee);
+        } catch (DataAccessException e) {
+            employee.setFirstName(oldFirstName);
+            throw new DataAccessException("Error updating employee's first name");
+        }
+    }
+
+    /*
+     * Update employee's last name
+     */
+    public void updateEmployeeLastName(Employee employee, String lastName) throws DataAccessException {
+        String oldLastName = employee.getLastName();
+        try {
+            employee.setLastName(lastName);
+            employeeDB.updateEmployee(employee);
+        } catch (DataAccessException e) {
+            employee.setLastName(oldLastName);
+            throw new DataAccessException("Error updating employee's last name");
+        }
+    }
+
+    /*
+     * Update employee's email
+     */
+    public void updateEmployeeEmail(Employee employee, String email) throws DataAccessException {
+        String oldEmail = employee.getEmail();
+        try {
+            employee.setEmail(email);
+            employeeDB.updateEmployee(employee);
+        } catch (DataAccessException e) {
+            employee.setEmail(oldEmail);
+            throw new DataAccessException("Error updating employee's email");
+        }
+    }
+
+    /*
+     * Update employee's password
+     */
+    public void updateEmployeePassword(Employee employee, String password) throws DataAccessException {
+        String oldPasswordHash = employee.getPasswordHash();
+        try {
+            String newPasswordHash = BCrypt.hashpw(password, BCrypt.gensalt());
+            employee.setPasswordHash(newPasswordHash);
+            employeeDB.updateEmployee(employee);
+        } catch (DataAccessException e) {
+            employee.setPasswordHash(oldPasswordHash);
+            throw new DataAccessException("Error updating employee's password");
+        }
+    }
+
+    /*
      * Delete employee
      */
     public void deleteEmployee(Employee employee) throws DataAccessException {
