8bb01cde7d356f9fd945cbc03465344662500fee
Daniels Kanepe <danixkanepe@gmail.com>
2022-04-13 18:29:29 +0200

    Implemented institution model

---
+++ src/model/Institution.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Institution {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Institution(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Institution{" + "id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + '}';
+    }
+}
