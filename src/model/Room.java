664b72576b32a60eac8a820205eebb8645bc2a28
Daniels Kanepe <danixkanepe@gmail.com>
2022-04-13 17:25:51 +0200

    Implemented 'Room' model

---
+++ src/model/Room.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Room {
+    private int roomID;
+    private boolean isOutOfService;
+    private RoomCategory roomCategory;
+
+    public Room(int roomID, RoomCategory roomCategory) {
+        if (roomCategory == null) {
+            throw new IllegalArgumentException("Room category cannot be null");
+        }
+
+        this.roomID = roomID;
+        this.roomCategory = roomCategory;
+        this.isOutOfService = false;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public boolean isOutOfService() {
+        return isOutOfService;
+    }
+
+    public void setOutOfService(boolean outOfService) {
+        isOutOfService = outOfService;
+    }
+
+    public RoomCategory getRoomCategory() {
+        return roomCategory;
+    }
+
+    public void setRoomCategory(RoomCategory roomCategory) {
+        this.roomCategory = roomCategory;
+    }
+
+    public String toString() {
+        return "Room ID: " + this.roomID + " - " + this.roomCategory.toString();
+    }
+}
+
