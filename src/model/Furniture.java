ab4d8cd8f86eec8a3cb7bffba7297f8245cbaed6
Daniels Kanepe <danixkanepe@gmail.com>
2022-04-13 17:35:43 +0200

    Implemented furniture model

---
+++ src/model/Furniture.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Furniture {
+    private String name;
+    private String description;
+    private int quantity;
+
+    public Furniture(String name, String description, int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity must be positive");
+        }
+
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
