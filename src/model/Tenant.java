fd54d314b6f0ea1c0a824c12ece5ebab69080785
Daniels Kanepe <danixkanepe@gmail.com>
2022-04-13 17:43:39 +0200

    implemented tenant model

---
+++ src/model/Tenant.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Tenant {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public Tenant(int id, String firstName, String lastName, String email, String phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
