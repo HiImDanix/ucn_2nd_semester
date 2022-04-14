1936e0475d42ce2c3a12dd76d3ea9dee236a6d13
Daniels Kanepe <danixkanepe@gmail.com>
2022-04-14 23:05:18 +0200

    Implemented images

---
+++ src/gui/Images.java
@@ -0,0 +1,56 @@
+package gui;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public enum Images {
+    // Here are the images that will be used in the program.
+    ADD_ITEM("images/add_item.png"),
+    MOON("images/moon.png"),
+    SUN("images/sun.png");
+
+    private final String path;
+    private final String defaultPath = "images/default.png";
+
+    // Take path of image
+    Images(String path) {
+        this.path = path;
+    }
+
+    // Get ImageIcon
+    public ImageIcon getImageIcon() {
+        // If image at path is not found, return default image icon
+        ImageIcon imageIcon = new ImageIcon(path);
+        if (imageIcon.getImageLoadStatus() != MediaTracker.COMPLETE) {
+            imageIcon = new ImageIcon(defaultPath);
+        }
+
+        if (imageIcon.getImageLoadStatus() == MediaTracker.COMPLETE) {
+            return imageIcon;
+        } else {
+            System.out.println("Image not found: " + path);
+            return null;
+        }
+    }
+
+    // Get resized image
+    public ImageIcon getImageIcon(int width, int height) {
+        ImageIcon imageIcon = getImageIcon();
+        // resize only if size is different
+        if (imageIcon.getIconWidth() != width || imageIcon.getIconHeight() != height) {
+            Image image = imageIcon.getImage();
+            image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            imageIcon = new ImageIcon(image);
+        }
+        return imageIcon;
+    }
+
+    // get image and resize to JComponent font's size
+    public ImageIcon getImageIcon(JComponent component) {
+        int fontSize = component.getFont().getSize();
+        return getImageIcon(fontSize, fontSize);
+
+    }
+}
+
