f8d96dc59bee0addfdda6a6104fac3f77c967d32
4ndris1114 <10407531@ucn.dk>
2022-04-15 22:50:36 +0200

    tenantdbif

---
+++ src/dal/TenantDBIF.java
@@ -0,0 +1,43 @@
+package dal;
+
+import java.util.List;
+
+import db.DataAccessException;
+import model.Tenant;
+
+public interface TenantDBIF {
+
+	/**
+	 * Get tenant by id
+	 * @param id
+	 * @return tenant found
+	 * @throws DataAccessException
+	 */
+	Tenant getById(int id) throws DataAccessException;
+	
+	//Tenant getByName(String name) throws DataAccessException;
+	
+	/**
+	 * Add a new tenant
+	 * @param t
+	 * @return tenant id
+	 * @throws DataAccessException
+	 */
+	int add(Tenant t) throws DataAccessException;
+	
+	/**
+	 * Update tenant
+	 * @param t
+	 * @throws DataAccessException
+	 */
+	void update(Tenant t) throws DataAccessException;
+	
+	/**
+	 * Delete tenant
+	 * @param t
+	 * @throws DataAccessException
+	 */
+	void delete(Tenant t) throws DataAccessException; 
+	
+	List<Tenant> getAll() throws DataAccessException;
+}
