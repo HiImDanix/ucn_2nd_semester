9d46ee56c6540907e2aff7c49b6a13d5815a7b6f
Daniels Kanepe <danixkanepe@gmail.com>
2022-05-04 23:01:41 +0200

    Implemented: tenantDB tests

--- src/test/dal/TestTenantDB.java
+++ src/test/dal/TestTenantDB.java
@@ -9,10 +9,7 @@ import db.DataAccessException;
 import model.Contract;
 import model.StudyProof;
 import model.Tenant;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -34,8 +31,19 @@ public class TestTenantDB {
         dataCtrl.clear();
     }
 
+    private boolean tenantsAreEqual(Tenant t1, Tenant t2) {
+        return t1.getID() == t2.getID() &&
+                t1.getFirstName().equals(t2.getFirstName()) &&
+                t1.getLastName().equals(t2.getLastName()) &&
+                t1.getEmail().equals(t2.getEmail()) &&
+                t1.getPhone().equals(t2.getPhone()) &&
+                t1.getStudyProof() == t2.getStudyProof() &&
+                t1.getContracts().equals(t2.getContracts());
+    }
+
     @Test()
     void testAddGetTenant() throws DataAccessException {
+        // Arrange
         String firstName = "Daniels";
         String lastName = "Kanepe";
         String email = "danixkanepe@gmail.com";
@@ -43,31 +51,78 @@ public class TestTenantDB {
         StudyProof studyProof = null;
         List<Contract> contracts = Collections.emptyList();
 
+        // Act
         Tenant tenant = new Tenant(-1, firstName, lastName, email, phone, studyProof, contracts);
         int tenantID = tenantDB.add(tenant);
-
+        tenant.setId(tenantID);
         Tenant dbTenant = tenantDB.getById(tenantID);
-        assert dbTenant.getID() == tenantID;
-        assert dbTenant.getFirstName().equals(firstName);
-        assert dbTenant.getLastName().equals(lastName);
-        assert dbTenant.getEmail().equals(email);
-        assert dbTenant.getPhone().equals(phone);
-        assert dbTenant.getStudyProof() == null;
-        assert dbTenant.getContracts().isEmpty();
+
+        // Assert
+        Assertions.assertTrue(tenantsAreEqual(tenant, dbTenant));
     }
 
     @Test()
     void testUpdateTenant() throws DataAccessException {
-        // TODO: Implement this test
+        // TODO: Implement this test when CRUD is implemented
     }
 
     @Test()
     void testDeleteTenant() throws DataAccessException {
+        // TODO: Implement this test when CRUD is implemented
     }
 
     @Test()
-    void testGetAllTenants() throws DataAccessException {
-        // TODO: Implement this test
+    void testGetAllTenantsNone() throws DataAccessException {
+        // Arrange
+        int expected = 0;
+
+        // Act
+        List<Tenant> tenants = tenantDB.getAll();
+
+        // Assert
+        assert tenants.size() == expected;
+    }
+
+    @Test()
+    void testGetAllTenantsOneTenant() throws DataAccessException {
+
+        // Arrange
+        Tenant tenant = new Tenant(-1, "Daniels", "Kanepe", "danixkanepe@gmail.com",
+                "1234567890", null, Collections.emptyList());
+        int tenantID = tenantDB.add(tenant);
+        tenant.setId(tenantID);
+
+        // Act
+        List<Tenant> tenants = tenantDB.getAll();
+
+
+        // Assert
+        Tenant dbTenant = tenants.get(tenants.size() - 1);
+        Assertions.assertTrue(tenantsAreEqual(tenant, dbTenant));
+    }
+
+    @Test()
+    void testGetAllTenantsTwoTenants() throws DataAccessException {
+
+        // Arrange
+        Tenant tenant1 = new Tenant(-1, "Daniels", "Kanepe", "danixkanepe@gmail.com",
+                "1234567890", null, Collections.emptyList());
+        Tenant tenant2 = new Tenant(-1, "Daniels", "Kanepe", "danixkanepe@gmail.com",
+                "1234567890", null, Collections.emptyList());
+        int tenantID1 = tenantDB.add(tenant1);
+        int tenantID2 = tenantDB.add(tenant2);
+        tenant1.setId(tenantID1);
+        tenant2.setId(tenantID2);
+
+        // Act
+        List<Tenant> tenants = tenantDB.getAll();
+
+        // Assert
+        Tenant dbTenant1 = tenants.get(tenants.size() - 2);
+        Tenant dbTenant2 = tenants.get(tenants.size() - 1);
+        Assertions.assertTrue(tenants.size() == 2);
+        Assertions.assertTrue(tenantsAreEqual(tenant1, dbTenant1));
+        Assertions.assertTrue(tenantsAreEqual(tenant2, dbTenant2));
     }
 
     @AfterAll
