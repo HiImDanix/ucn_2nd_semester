e68c721336bee594e8365a169b4a7db265176f34
Daniels Kanepe <danixkanepe@gmail.com>
2022-05-12 11:37:15 +0200

    Finished implementing DBConnection tests

--- src/test/db/TestDBConnection.java
+++ src/test/db/TestDBConnection.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -14,40 +15,47 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestDBConnection {
 
-    static DBConnection dbCon = null;
-
-    @BeforeAll
-    static void setUp() throws DataAccessException {
-        dbCon = DBConnection.getInstance();
+    @AfterAll
+    static void cleanUp() throws DataAccessException {
+        DBConnection.getInstance().closeConnection();
     }
 
+    @Test
+    public void connect() throws DataAccessException {
+        // Get connection
+        Connection con = DBConnection.getInstance().getConnection();
 
-    @Test()
-    public void connectAndReconnect() throws SQLException, DataAccessException {
+        // test that the connection is not null
+        assertNotNull(con, "Connection is null");
+
+        // Retrieve data from the database (should not throw an exception)
+        Assertions.assertDoesNotThrow(() -> con.createStatement().executeQuery("SELECT 1"));
+    }
+
+    @Test
+    public void disconnect() throws DataAccessException {
         // Get the connection
-        assertNotNull(dbCon.getConnection(), "Connection is null");
-
-        // Retrieve data from the database
-        dbCon.getConnection().createStatement().executeQuery("SELECT 1");
+        Connection con = DBConnection.getInstance().getConnection();
 
         // Disconnect
-        dbCon.closeConnection();
+        DBConnection.getInstance().closeConnection();
 
         // Check that the connection is closed
-        Assertions.assertThrows(SQLException.class,
-                () -> dbCon.getConnection().createStatement().executeQuery("SELECT 1"));
-
-        // Get the connection
-        dbCon = DBConnection.getInstance();
-        assertNotNull(dbCon.getConnection(), "Connection is null");
-
-        // Retrieve data from the database
-        dbCon.getConnection().createStatement().executeQuery("SELECT 1");
+        Assertions.assertThrows(SQLException.class, () -> con.createStatement().executeQuery("SELECT 1"));
     }
 
-    @AfterAll
-    static void cleanUp() throws DataAccessException, SQLException {
+    @Test()
+    public void Reconnect() throws DataAccessException {
+        // Get the connection
+        DBConnection.getInstance().getConnection();
+
+        // Disconnect
         DBConnection.getInstance().closeConnection();
+
+        assertNotNull(DBConnection.getInstance().getConnection(), "Connection is null");
+
+        // Try retrieving data from the database
+        Assertions.assertDoesNotThrow(() -> DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT 1"));
     }
 
 }
