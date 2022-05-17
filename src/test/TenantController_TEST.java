40cb9d928db128d4b8d6cee693fceb60fc4badc5
4ndris1114 <10407531@ucn.dk>
2022-05-17 20:04:37 +0200

    Fixing tenant controller test class

--- src/test/TenantController_TEST.java
+++ src/test/TenantController_TEST.java
@@ -2,49 +2,74 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 //import java.util.List;
+import java.util.List;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import controller.ContractController;
+import controller.DBController;
+import controller.EmployeeController;
 import controller.RoomCategoryController;
 import controller.RoomController;
+import controller.SessionController;
 import controller.TenantController;
+import db.DBConnection;
 import db.DataAccessException;
+import gui.Common;
 import model.Contract;
 import model.Room;
 import model.RoomCategory;
 import model.StudyProof;
 import model.Tenant;
+import model.modelIF;
 
-class TenantController_TEST {
+class TenantController_TEST implements TestIF<Tenant> {
 	
-	private TenantController tCtr;
-	private RoomCategoryController rcCtr;
+	private static TenantController tCtr;
+	private static RoomCategoryController rcCtr;
+	private static ContractController cCtr;
 	//private List<Contract> contracts;
 	private StudyProof studyP;
-	private RoomController rCtr;
-	ArrayList<Tenant> tenants;
+	private static RoomController rCtr;
+	private DBConnection dbCon;
+	private static DBController dbCtrl;
+	private StudyProof studyP2;
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@BeforeAll
+	static void setUpBeforeClass() {
 		tCtr = new TenantController();
 		rcCtr = new RoomCategoryController();
+		cCtr = new ContractController();
 		rCtr = new RoomController();
-		tenants = new ArrayList<>();
-		// StudyProof studyP = new StudyProof(1, "path/path/path", LocalDate.parse("2022-10-10"));
-		/* if (tCtr.getTenantById(1) != null) {
-			tCtr.removeTeanant(tCtr.getTenantById(1));
-		} */
+		dbCtrl = new DBController();
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		dbCtrl.clear();
+		dbCon = DBConnection.getInstance();
+		studyP2 = new StudyProof(1, "path/path/path", LocalDate.parse("2023-10-10"));
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@AfterAll
+	static void tearDownAfterClass() throws DataAccessException, IOException {
+		dbCtrl.clear();
+		dbCtrl.addDemoData();
+		DBConnection.getInstance().closeConnection();
 	}
+	
 
 	@Test
 	void testAddTenant() throws DataAccessException {
@@ -56,35 +81,55 @@ class TenantController_TEST {
 	
 	@Test
 	void testGetTenantById() throws DataAccessException {
-		assertNull(tCtr.getTenantById(1)); // Empty test
-		assertEquals(tCtr.addTenant("testName", "name1", "email1@email.com", "+4552525252", studyP), tCtr.getTenantById(1)); // Tenant added = tenant found
+		//Arrange
+		Tenant addedTenant = new Tenant(-1, "testName", "name1", "email1@email.com", "+4552525252", studyP, null);
+		Tenant retrievedTenant = null;
+		
+		//Act
+		addedTenant = tCtr.addTenant(addedTenant);
+		retrievedTenant = tCtr.getTenantById(1);
+		
+		//Assert
+		assertTrue(addedTenant.getID() != -1);
+		assertTrue(compareObjects(addedTenant, retrievedTenant)); // Tenant added = tenant found
 		/* tCtr.removeTenant(tCtr.getTenantById(1)); */
 	}
 
 	@Test
 	void testGetAllTenants() throws DataAccessException {
-		assertNull(tCtr.getAllTenants()); // empty test
-		tCtr.addTenant("testName", "name1", "email1@email.com", "+4552525252", studyP);
-		tCtr.addTenant("testName", "name2", "email1@email.com", "+4552522222", studyP);
-		assertEquals(2, tCtr.getAllTenants()); // 2 created = 2 found
+		assertTrue(tCtr.getAllTenants().isEmpty()); // empty test
+		//Arrange
+		Tenant tenant1 = new Tenant(-1, "testName", "name1", "email1@email.com", "+4552525252", studyP, null);
+		Tenant tenant2 = new Tenant(-1, "testName2", "name2", "email2@email.com", "+4552525252", studyP, null);
+		//Act
+		tCtr.addTenant(tenant1);
+		tCtr.addTenant(tenant2);
+		
+		//Assert
+		assertEquals(2, tCtr.getAllTenants().size()); // 2 created = 2 found
+		assertTrue(compareObjects(tenant1, tCtr.getAllTenants().get(0)));
+		assertTrue(compareObjects(tenant2, tCtr.getAllTenants().get(1)));
 		/* tCtr.removeTenant(tCtr.getTenantById(1)); // Clean up
 		tCtr.removeTenant(tCtr.getTenantById(2)); */ // Clean up
 	}
 
 	@Test
 	void testGetTenantsByContractID() throws DataAccessException { // Problem - lists don't match - ArrayList / List<>
-		RoomCategory cat = rcCtr.getRoomCategoryById(1); // create room category to create a room
-		tenants.add(tCtr.getTenantById(1)); // Make a list with the desired tenant, to compare with result
-		Room room = new Room(0, cat, false, new ArrayList<>()); // create a room to create contract
-		tCtr.addTenant("testName", "name1", "email1@email.com", "+4552525252", studyP); // create tenant to assign to contract
-		tCtr.getTenantById(1).addContract(new Contract(-1, true, LocalDate.parse("2022-01-01"), room, new ArrayList<>(), 
-				Collections.emptyList(), Collections.emptyList(), null)); // Assign tenant & room to contract
-		assertEquals(tenants, tCtr.getTenantsByContractID(1)); // perform test
-		// rcCtr.removeRoomCategory(rcCtr.getRoomCategoryById(1))  // remove category
-		rCtr.deleteRoom(room); // delete room
-		// tCtr.removeTenant(tCtr.getTenantById(1)); // delete tenant
+		//Arrange
+		RoomCategory cat = rcCtr.addRoomCategory("testCategory", "testDescription", BigDecimal.valueOf(3000), BigDecimal.valueOf(0), BigDecimal.valueOf(300), 1, 30); // add room category to create a room
+		Room room = rCtr.addRoom(cat, false); // create room to assign to contract
+		Tenant addedTenant = tCtr.addTenant("testName", "name1", "email1@email.com", "+4552525252", studyP); // create tenant to assign to contract
+		
+		//Act
+		cCtr.addContract(LocalDate.parse("2023-01-01"), Arrays.asList(addedTenant), room, true);
+		
+		//Assert
+		assertTrue(tCtr.getTenantsByContractID(0).size() == 1); // perform test
+		System.out.println(addedTenant.getLastName());
+		System.out.println(tCtr.getTenantsByContractID(0).get(0).getLastName());
+		assertTrue(compareObjects(addedTenant, tCtr.getTenantsByContractID(0).get(0)));
 	}
-
+/*
 	@Test
 	void testUpdateTenant() throws DataAccessException {
 		tCtr.addTenant("testName", "name1", "email@email.com", "+4552525252", studyP);
@@ -96,5 +141,15 @@ class TenantController_TEST {
 		assertEquals("+2222222222", tenant.getPhone());
 		// tCtr.removeTenant(tCtr.getTenantById(1)); // Clean up
     }
+*/
+	@Override
+	public boolean compareObjects(Tenant obj, Tenant obj2) {
+		return
+				obj.getID() == obj2.getID() &&
+				obj.getFirstName().equals(obj2.getFirstName()) &&
+				obj.getLastName().equals(obj2.getLastName()) &&
+				obj.getEmail().equals(obj2.getEmail()) &&
+				obj.getPhone().equals(obj2.getPhone());
+	}
 
 }
